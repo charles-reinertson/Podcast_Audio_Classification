8762d0beecc00440c00af02edf963fe17cf1a56a
charles-reinertson <68883565+charles-reinertson@users.noreply.github.com>
2021-03-27 07:31:25 -0700

    Update mode.py

--- mode.py
+++ mode.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
-from data_prep import feature_engineering
+from data_prep import partition_data
 from transcript_feature_extraction import *
 import torch
 import torch.nn as nn
@@ -13,21 +13,7 @@ from sklearn.model_selection import train_test_split
 
 # CITATION: Some of these function are taken from my own (Charle's Reinertson) EECS 445 Project 2. 
 
-def save_dataframe(test_file_location, train_file_location, validate_file_location):
-    # get the test, train, and validate pandas dataframes and save them in a .pkl file.
-    # Can later get back these pandas dataframes with "df = pd.read_pickle(file_name)"
-    # test_file_location: location to store the test pandas dataframe
-    # train_file_location: location to store the train pandas dataframe
-    # validate_file_location: location to store the validate pandas dataframe
 
-    train, test, validate = feature_engineering()
-
-    # save dataframe
-    test.to_pickle(test_file_location)
-    train.to_pickle(train_file_location)
-    validate.to_pickle(validate_file_location)
-
-    return None
 
 def _train_epoch(data_loader, model, criterion, optimizer):
     """
@@ -172,45 +158,41 @@ class NNetwork(nn.Module):
 def main():
     # DELETE. JUST FOR FAKE EXAMPLE DATA
     #_______________________________________________________________________________________________________________________
-    amazon = pd.read_csv('sentiment labelled sentences/amazon_cells_labelled.txt', delimiter = "\t", quoting=3, header=None)
-    amazon.columns = ['text', 'label']
-    features = {'anger': 0, 'anticipation': 1, 'disgust': 2, 'fear': 3, 'joy': 4, 'sadness': 5,
-            'surprise': 6, 'trust': 7, 'negative': 8, 'positive': 9, 'CC': 10, 'IN': 11, 'JJR': 12,
-            'JJS': 13, 'PRP': 14}
-    # getting the feature representation of each transcript in the above format
-    transcript_features1 = getFeatures(amazon, features)
-    # bag of words
-    word_dict = extract_dictionary(amazon)
-    transcript_features2 = generate_feature_matrix(amazon, word_dict)
-    # combine bag of words and emotional feature representation. We can do this becuase both are normalized
-    transcript_features = np.hstack((transcript_features1, transcript_features2))
+    # amazon = pd.read_csv('sentiment labelled sentences/amazon_cells_labelled.txt', delimiter = "\t", quoting=3, header=None)
+    # amazon.columns = ['text', 'label']
+    # transcript_features = process_transcripts(amazon.text, amazon.text)
 
-    categories = np.random.randint(11, size=transcript_features.shape[0]) 
+    # categories = np.random.randint(11, size=transcript_features.shape[0]) 
 
-    df = pd.DataFrame({'categories': categories, 'transcript_features': list(transcript_features)}, columns=['categories', 'transcript_features'])
-    print(df.head())
+    # df = pd.DataFrame({'categories': categories, 'transcript_features': list(transcript_features)}, columns=['categories', 'transcript_features'])
+    # print(df.head())
 
-    train, test = train_test_split(df, test_size=0.2)
+    # train, test = train_test_split(df, test_size=0.2)
     # train = train.reset_index(drop=True)
-    # test = train.reset_index(drop=True)
+    # test = test.reset_index(drop=True)
     #_______________________________________________________________________________________________________________________
     # DELETE ALL ABOVE
 
 
-    """
+    
     test_file_location = "./data/test_dataframe.pkl"
     train_file_location = "./data/train_dataframe.pkl"
     validate_file_location = "./data/validate_dataframe.pkl"
 
-    # COMMENT OUT IF DATAFRAME ALREADY SAVED IN .pkl file
-    # save_dataframe(test_file_location, train_file_location, validate_file_location)
+  
 
     test = pd.read_pickle(test_file_location)
     train = pd.read_pickle(train_file_location)
     validate = pd.read_pickle(validate_file_location)
-    """
 
-    
+    # change indexes so right after train the tests indexes start, and right after the test indexes the validate indexes start
+    test.index = np.arange(train.shape[0], train.shape[0] + test.shape[0])
+    validate.index = np.arange(train.shape[0] + test.shape[0], train.shape[0] + test.shape[0] + validate.shape[0])
+
+    # stack train, test, and validate on top one another in that exact order
+    df = pd.concat([train, test, validate])
+    # df = pd.concat([train, test])
+
     max_epochs = 40
     partition = {}
     labels = {}
@@ -221,6 +203,8 @@ def main():
         labels[index] = int(train[train.index == index]['categories'])
     for index in test.index:
         labels[index] = int(test[test.index == index]['categories'])
+    # for index in validate.index:
+    #     labels[index] = int(validate[validate.index == index]['categories'])
 
 
 
@@ -228,7 +212,7 @@ def main():
     training_set = Dataset(partition['train'], labels, df['transcript_features'])
     training_generator = torch.utils.data.DataLoader(training_set, batch_size=64, shuffle=True)
 
-    test_set = Dataset(partition['test'], labels, df['transcript_features'])
+    test_set = Dataset(partition['test'], labels, test['transcript_features'])
     test_generator = torch.utils.data.DataLoader(test_set, batch_size=64, shuffle=True)
 
     # Loop over epochs
